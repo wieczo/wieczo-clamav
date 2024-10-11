8f44f1cb5cd7b33b2184b64fb14ab0e4293349c1
Thomas Wieczorek <wieczo.yo@googlemail.com>
2024-10-11 13:46:24 +0200

    - WordPress.org plugin review: better uninstall routine

--- src/ClamAV/Table.php
+++ src/ClamAV/Table.php
@@ -8,15 +8,13 @@ class Table {
 
 		$tableName      = $wpdb->prefix . Config::TABLE_LOGS;
 		$charsetCollate = $wpdb->get_charset_collate();
-		$ddl            = <<<SQL
-CREATE TABLE $tableName (
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(255) NOT NULL,
-    filename VARCHAR(255) NOT NULL,
-    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY  (id)
-) $charsetCollate;
-SQL;
+		$ddl            = "CREATE TABLE $tableName (
+						    id mediumint(9) NOT NULL AUTO_INCREMENT,
+						    user_name VARCHAR(255) NOT NULL,
+						    filename VARCHAR(255) NOT NULL,
+						    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+						    PRIMARY KEY  (id)
+						) $charsetCollate;";
 
 		// Required WordPress methods to create the table
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
@@ -28,11 +26,20 @@ SQL;
 	public function dropTable() {
 		global $wpdb;
 
-		// Tabellenname definieren
 		$tableName = $wpdb->prefix . Config::TABLE_LOGS;
 
-		// Tabelle löschen
+		// Delete table
 		// phpcs:ignore
 		$wpdb->query( $wpdb->prepare( "DROP TABLE IF EXISTS %s", $tableName ) );
+
+		// Delete options
+		$options = [
+			'host',
+			'port',
+			'timeout'
+		];
+		foreach ( $options as $option ) {
+			delete_option( 'clamav_' . $option );
+		}
 	}
 }
