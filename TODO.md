925710b830c50ae9b0a7a09dd4420175971aa29e
Thomas Wieczorek <wieczo.yo@googlemail.com>
2024-10-16 00:19:55 +0200

    - Removes tablesorter since the log table uses WP_List_Table which makes the tablesorter obsolete

--- TODO.md
+++ TODO.md
@@ -3,5 +3,4 @@
 * Split HTML and PHP in the class files
 * Notify admin when a user tried to upload a virus
 * Add cron to scan all files
-* Remove tablesorter.js & css
 * Icon for WordPress plugin directory
--- src/ClamAV/Enqueuer.php
+++ src/ClamAV/Enqueuer.php
@@ -7,25 +7,10 @@ use const WIECZOS_VIRUS_SCANNER_PLUGIN_DIR;
 class Enqueuer {
 	public function __construct() {
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue' ] );
-
-		add_action( 'admin_footer', [ $this, 'addTableSortingJS' ] );
 	}
 
 	public function enqueue(): void {
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'jquery-ui-sortable' );
-		wp_enqueue_script( 'tablesorter', plugins_url( 'assets/js/jquery.tablesorter.min.js', WIECZOS_VIRUS_SCANNER_PLUGIN_DIR ), [ 'jquery' ], "2.32.0", true );
-
-		wp_enqueue_style( 'tablesorter-css', plugins_url( 'assets/css/jquery.tablesorter.min.css', WIECZOS_VIRUS_SCANNER_PLUGIN_DIR ), [], "2.32.0" );
-	}
-
-	function addTableSortingJS(): void {
-		wp_add_inline_script('init-tablesorter', "
-		<script type='text/javascript'>
-            jQuery(document).ready(function ($) {
-                $('.tablesorter').tablesorter();
-            });
-        </script>
-        ");
 	}
 }
